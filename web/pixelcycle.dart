9eb7279df9cb3ca274c251947f407ed9796a5656
Brian Slesinsky <brian-git@slesinsky.org>
2013-05-11 23:13:01 -0700

    refactor to get GridModel and GridView

--- web/pixelcycle.dart
+++ web/pixelcycle.dart
@@ -1,14 +1,13 @@
 import 'dart:html';
 import 'dart:async' as async;
 
-class PixelRect {
+class GridModel {
   int width;
   int height;
   int pixelsize;
   List<String> pixels;
-  bool willRender = false;
   
-  PixelRect(this.width, this.height, this.pixelsize, String color) {
+  GridModel(this.width, this.height, this.pixelsize, String color) {
     pixels = new List<String>(width * height);
     for (num i = 0; i < pixels.length; i++) {
       pixels[i] = color;
@@ -51,56 +50,64 @@ class PixelRect {
       }
     }
   }
-  
-  void onMouseDraw(MouseEvent e, String color, CanvasRenderingContext2D c) {
-    int x = (e.offsetX / pixelsize).toInt();
-    int y = (e.offsetY / pixelsize).toInt();
-    if (!set(x, y, color)) {
-      return;
-    }
-    if (!willRender) {
-      window.requestAnimationFrame((t) {
-        render(c);
-        willRender = false;
-      });
-      willRender = true;
-    }
-  }  
 }
 
 typedef void cancelFunc();
 
-cancelFunc startDrawing(PixelRect p, CanvasElement elt) {
-  var sub = elt.onMouseMove.listen((MouseEvent e) {
-    p.onMouseDraw(e, "#0F0", elt.context2D);
-  });
-  return sub.cancel;
+class GridView {
+  GridModel m;
+  CanvasElement elt;
+  bool willRender = false;
+  cancelFunc stopDrawing = () {};
+  
+  GridView(this.m) {
+    elt = new CanvasElement();
+    elt.width = m.renderWidth();
+    elt.height = m.renderWidth();
+    
+    elt.onMouseDown.listen((MouseEvent e) {
+      if (e.button == 0) {
+        var sub = elt.onMouseMove.listen((MouseEvent e) {
+          paint(e, "#0F0");
+        });
+        stopDrawing = sub.cancel;
+        e.preventDefault(); // don't change the cursor
+      }
+    });
+    
+    query("body").onMouseUp.listen((MouseEvent e) {
+      stopDrawing();
+    });
+    
+    elt.onMouseOut.listen((MouseEvent e) {
+      stopDrawing();      
+    });
+  }
+
+  void renderAsync() {
+    if (willRender) {
+      return;
+    }
+    window.requestAnimationFrame((t) {
+      m.render(elt.context2D);
+      willRender = false;
+    });
+    willRender = true;   
+  }
+  
+  void paint(MouseEvent e, String color) {
+    int x = (e.offsetX / m.pixelsize).toInt();
+    int y = (e.offsetY / m.pixelsize).toInt();
+    if (m.set(x, y, color)) {
+      renderAsync();
+    }
+  }
 }
 
 void main() {
   
-  PixelRect p = new PixelRect(32, 32, 10, "#000");
-  
-  CanvasElement elt = new CanvasElement();
-  elt.width = p.renderWidth();
-  elt.height = p.renderWidth();
-  query("#canvas").append(elt);
-  
-  CanvasRenderingContext2D c = elt.context2D;
-  p.render(c);
-  
-  cancelFunc stopDrawing = () {};
-  elt.onMouseDown.listen((MouseEvent e) {
-    if (e.button == 0) {
-      stopDrawing = startDrawing(p, elt);
-      e.preventDefault(); // don't change the cursor
-    }
-  });
-  
-  query("body").onMouseUp.listen((MouseEvent e) {
-    stopDrawing();
-  });
-  elt.onMouseOut.listen((MouseEvent e) {
-    stopDrawing();      
-  });
+  GridModel p = new GridModel(32, 32, 10, "#000");
+  GridView v = new GridView(p);
+  query("#canvas").append(v.elt);
+  v.renderAsync();  
 }
