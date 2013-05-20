cd7ff2c6c1c8503776c2de6aea9d80a97ffb2d36
Brian Slesinsky <brian-git@slesinsky.org>
2013-05-19 21:46:33 -0700

    add keyboard input

--- web/pixelcycle.dart
+++ web/pixelcycle.dart
@@ -3,6 +3,7 @@ import 'dart:html';
 import 'dart:async' as async;
 
 part 'palette.dart';
+part 'player.dart';
 
 class GridModel {
   final PaletteModel palette;
@@ -157,82 +158,6 @@ class MovieModel {
   }
 }
 
-class PlayerModel {
-  final MovieModel movie;
-  int frame = 0;
-  async.Stream<int> onFrameChange;
-  async.EventSink<int> _onFrameChangeSink;
-  
-  bool playing = false;
-  int fps;
-  async.Stream<PlayerModel> onSettingChange;
-  async.EventSink<PlayerModel> _onSettingChangeSink;
-  async.Timer _ticker;
-  
-  PlayerModel(this.movie, this.fps) {
-    var controller = new async.StreamController<int>();
-    onFrameChange = controller.stream.asBroadcastStream();
-    _onFrameChangeSink = controller.sink;    
-    var controller2 = new async.StreamController<PlayerModel>();
-    onSettingChange = controller2.stream.asBroadcastStream();
-    _onSettingChangeSink = controller2.sink;    
-  }
-  
-  void setFrame(int frameIndex) {
-    this.frame = frameIndex;
-    _onFrameChangeSink.add(frameIndex);
-  }
-  
-  void setFramesPerSecond(int newValue) {
-    if (fps != newValue) {
-      fps = newValue;
-      _onSettingChangeSink.add(this);
-    }
-  }
-  
-  void setPlaying(bool newValue) {
-    if (playing == newValue) {
-      return;
-    }
-    playing = newValue;
-    _onSettingChangeSink.add(this);
-    if (playing) {
-      _tick();
-    } else {
-      _cancelTick();
-    }
-  }
-
-  void _scheduleTick() {
-    _cancelTick();
-    int delay = (1000/fps).toInt();
-    _ticker = new async.Timer(new Duration(milliseconds: delay), _tick);    
-  }
-  
-  void _cancelTick() {
-    if (_ticker != null) {
-      _ticker.cancel();
-      _ticker = null;
-    }    
-  }
-  
-  void _tick() {
-    if (!playing) {
-      return;
-    }
-    _scheduleTick();
-    step();
-  }
-  
-  void step() {
-    int next = frame + 1;
-    if (next >= movie.frames.length) {
-      next = 0;
-    }
-    setFrame(next);    
-  }
-}
-
 class FrameListView {
   final PlayerModel player;
   final Element elt = new DivElement();
@@ -266,41 +191,6 @@ class FrameListView {
   }
 }
 
-class PlayerView {
-  final PlayerModel player;
-  final Element elt = new DivElement();
-  final ButtonElement play;
-  final RangeInputElement slider;
-  
-  PlayerView(this.player) :
-    play = new ButtonElement(),
-    slider = new RangeInputElement()
-  {
-    slider ..min = "1" ..max = "60";
-    
-    elt ..append(play) ..append(slider);
-      
-    play.onClick.listen((e) {
-      player.setPlaying(!player.playing);
-    });
-    slider.onChange.listen((e) {
-      player.setFramesPerSecond(int.parse(slider.value));  
-    });
-    
-    player.onSettingChange.listen((e) => render());  
-    render();
-  }
-  
-  void render() {
-    if (player.playing) {
-      play.text = "Stop";
-    } else {
-      play.text = "Play";
-    }
-    slider.value = player.fps.toString();    
-  }
-}
-
 void main() {
   PaletteModel pm = new PaletteModel.standard();
   pm.select(51);
@@ -319,6 +209,25 @@ void main() {
   query("#player").append(new PlayerView(player).elt);
   query("#grid").append(big.elt);
   query("#palette").append(new PaletteView(pm, pm.colors.length~/4).elt);
+  
+  document.onKeyDown.listen((KeyboardEvent e) {
+    switch (e.keyCode) {
+      case KeyCode.RIGHT:
+        player.step(1);
+        break;
+      case KeyCode.LEFT:
+        player.step(-1);
+        break;    
+    }
+  });
+  
+  document.onKeyPress.listen((KeyboardEvent e) {
+    switch (e.keyCode) {
+      case KeyCode.SPACE:
+        player.step(1);
+        break;
+    }
+  });
   
   player.setPlaying(true);
 }
